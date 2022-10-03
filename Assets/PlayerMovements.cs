8702c6fbb47451030e4ef0d5db49705658a7e870
Cheddae Grant <chedgrant@hotmail.com>
2022-10-03 11:37:16 -0400

    Add code to PlayerMovement file
    added codes to line 15 and 16
    private enum MovementState { idle, running, jumping, falling }
    private MovementState state = MovementState.idle;

--- Assets/PlayerMovements.cs
+++ Assets/PlayerMovements.cs
@@ -12,8 +12,11 @@ private float dirX = 0f;
  [SerializeField] private float moveSpeed = 7f;
  [SerializeField] private float jumpForce = 7f;
 
+private enum MovementState { idle, running, jumping, falling }
+private MovementState state = MovementState.idle;
+
     // Start is called before the first frame update
-   private void Start()
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
